ec9db816c37b23b181d4537624c70bdfd5fd0b5e
Mateo <mateoreynoso2009@gmail.com>
2022-09-27 22:11:45 -0300

    terminado

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,26 +1,32 @@
 import React from "react";
 import "./Card.css"
-import { Link } from "react-router-dom";
 
-export default function Card(props){
 
+/* export default function Card(props){ */
+    export default class Card extends React.Component {
+        constructor(props) {
+            super(props);
+            console.log(this.props)
+          }
+        render() {
     return(
         
         <div className="card" /* background-color="gray" */>
             
             {/* <Link className="link" to={"/home/" + props.id}> */}
             <div >
-                <h3 className="link">{props.name}</h3>
+                <h3 className="link">{this.props.name}</h3>
             </div>
             {/* </Link> */}
             <div>
-                <img className="img-c" src={props.flag} alt="flag not found" />
+                <img className="img-c" src={this.props.flag} alt="flag not found" />
             </div>
             <div className="continent">
-                <h5 >{props.id}</h5>
-                <h5 >{props.continent}</h5>
+                <h5 >{this.props.id}</h5>
+                <h5 >{this.props.continent}</h5>
 
             </div>
         </div>
     )
-}
+    }}
+
